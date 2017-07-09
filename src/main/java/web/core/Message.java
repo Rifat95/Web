5a782dff5ccc0764dcd25ff3e77487ab2d8e0aac
Rifat95 <islamrifat777@gmail.com>
2017-07-09 15:34:30 +0200

    Add core.Message

---
+++ src/main/java/web/core/Message.java
@@ -0,0 +1,23 @@
+package web.core;
+
+public final class Message {
+	public static final String INFO = "info";
+	public static final String SUCCESS = "message";
+	public static final String WARNING = "warning";
+
+	private String type;
+	private String content;
+
+	public Message(String type, String content) {
+		this.type = type;
+		this.content = content;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
